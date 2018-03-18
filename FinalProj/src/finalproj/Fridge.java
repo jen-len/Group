04729144db260ecebeea374d6b7fbcae6d69330e
DierksBentely <35244998+DierksBentely@users.noreply.github.com>
2018-03-18 19:18:53 -0400

    Delete Fridge.java

--- FinalProj/src/finalproj/Fridge.java
+++
@@ -1,8 +0,0 @@
-package finalproj;
-
-public class Fridge 
-{
-	// keep store of what the user has + how much time they have
-	// implement a list of 15 or 20 things that could be made from 
-	// common kitchen ingredients?
-}
